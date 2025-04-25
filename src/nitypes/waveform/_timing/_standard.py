1b843b1cf00eaecafdb65fdf14af5850381001ec
Brad Keryan <brad.keryan@ni.com>
2025-04-25 12:18:43 -0500

    waveform: Use override decorator and remove duplicated docstrings

--- src/nitypes/waveform/_timing/_standard.py
+++ src/nitypes/waveform/_timing/_standard.py
@@ -1,10 +1,16 @@
 from __future__ import annotations
 
 import datetime as dt
+import sys
 from collections.abc import Sequence
 from typing import ClassVar
 
 from nitypes.waveform._timing._base import BaseTiming, SampleIntervalMode
+
+if sys.version_info >= (3, 12):
+    from typing import override
+else:
+    from typing_extensions import override
 
 
 class Timing(BaseTiming[dt.datetime, dt.timedelta]):
@@ -17,68 +23,42 @@ class Timing(BaseTiming[dt.datetime, dt.timedelta]):
     _DEFAULT_TIME_OFFSET = dt.timedelta()
 
     empty: ClassVar[Timing]
+    """A waveform timing object with no timestamp, time offset, or sample interval."""
 
-    # TODO: can these be classmethods in BaseTiming?
+    @override
     @staticmethod
-    def create_with_no_interval(
+    def create_with_no_interval(  # noqa: D102 - Missing docstring in public method - override
         timestamp: dt.datetime | None = None, time_offset: dt.timedelta | None = None
     ) -> Timing:
-        """Create a waveform timing object with no sample interval.
-
-        Args:
-            timestamp: A timestamp representing the start of an acquisition or a related
-                occurrence.
-            time_offset: The time difference between the timestamp and the time that the first
-                sample was acquired.
-
-        Returns:
-            A waveform timing object.
-        """
         return Timing(SampleIntervalMode.NONE, timestamp, time_offset)
 
+    @override
     @staticmethod
-    def create_with_regular_interval(
+    def create_with_regular_interval(  # noqa: D102 - Missing docstring in public method - override
         sample_interval: dt.timedelta,
         timestamp: dt.datetime | None = None,
         time_offset: dt.timedelta | None = None,
     ) -> Timing:
-        """Create a waveform timing object with a regular sample interval.
-
-        Args:
-            sample_interval: The time difference between samples.
-            timestamp: A timestamp representing the start of an acquisition or a related
-                occurrence.
-            time_offset: The time difference between the timestamp and the time that the first
-                sample was acquired.
-
-        Returns:
-            A waveform timing object.
-        """
         return Timing(SampleIntervalMode.REGULAR, timestamp, time_offset, sample_interval)
 
+    @override
     @staticmethod
-    def create_with_irregular_interval(
+    def create_with_irregular_interval(  # noqa: D102 - Missing docstring in public method - override
         timestamps: Sequence[dt.datetime],
     ) -> Timing:
-        """Create a waveform timing object with an irregular sample interval.
-
-        Args:
-            timestamps: A sequence containing a timestamp for each sample in the waveform,
-                specifying the time that the sample was acquired.
-
-        Returns:
-            A waveform timing object.
-        """
         return Timing(SampleIntervalMode.IRREGULAR, timestamps=timestamps)
 
+    @override
     @staticmethod
     def _get_datetime_type() -> type[dt.datetime]:
         return dt.datetime
 
+    @override
     @staticmethod
     def _get_timedelta_type() -> type[dt.timedelta]:
         return dt.timedelta
 
+    @override
     @staticmethod
     def _get_default_time_offset() -> dt.timedelta:
         return Timing._DEFAULT_TIME_OFFSET
@@ -102,4 +82,3 @@ class Timing(BaseTiming[dt.datetime, dt.timedelta]):
 
 
 Timing.empty = Timing.create_with_no_interval()
-"""A waveform timing object with no timestamp, time offset, or sample interval."""
