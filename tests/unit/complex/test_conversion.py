1eaa28d7ae49ed728861303f901b63ee32b3191c
Brad Keryan <brad.keryan@ni.com>
2025-05-07 13:48:08 -0500

    tests: Add test cases for array shape

--- tests/unit/complex/test_conversion.py
+++ tests/unit/complex/test_conversion.py
@@ -105,6 +105,58 @@ def test___complex128_array_to_complex128_array___convert_complex___returns_orig
     assert value_out is value_in
 
 
+def test___2d_complexint32_array_to_complex128_array___convert_complex___preserves_shape() -> None:
+    value_in = np.array(
+        [[(1, 2), (3, -4)], [(-5, 6), (-7, -8)], [(9, 10), (11, 12)]], ComplexInt32DType
+    )
+
+    value_out = convert_complex(np.complex128, value_in)
+
+    # Use npt.NDArray because np.array() can't infer the array shape when type checking.
+    assert_type(value_out, npt.NDArray[np.complex128])
+    assert isinstance(value_out, np.ndarray) and value_out.shape == (3, 2)
+    assert [list(x) for x in value_out] == [
+        [1 + 2j, 3 - 4j],
+        [-5 + 6j, -7 - 8j],
+        [9 + 10j, 11 + 12j],
+    ]
+
+
+def test___2d_complex64_array_to_complex128_array___convert_complex___preserves_shape() -> None:
+    value_in = np.array([[1 + 2j, 3 - 4j], [-5 + 6j, -7 - 8j], [9 + 10j, 11 + 12j]], np.complex64)
+
+    value_out = convert_complex(np.complex128, value_in)
+
+    # Use npt.NDArray because np.array() can't infer the array shape when type checking.
+    assert_type(value_out, npt.NDArray[np.complex128])
+    assert isinstance(value_out, np.ndarray) and value_out.shape == (3, 2)
+    assert [list(x) for x in value_out] == [
+        [1 + 2j, 3 - 4j],
+        [-5 + 6j, -7 - 8j],
+        [9 + 10j, 11 + 12j],
+    ]
+
+
+def test___arrays_with_static_shape___convert_complex___preserves_static_and_runtime_shape() -> (
+    None
+):
+    # np.zeros() can infer the array shape when type checking because it takes a shape argument.
+    value_in_1d = np.zeros(3, np.complex64)
+    value_in_2d = np.zeros((4, 5), np.complex128)
+    value_in_3d = np.zeros((6, 7, 8), ComplexInt32DType)
+
+    value_out_1d = convert_complex(np.complex128, value_in_1d)
+    value_out_2d = convert_complex(ComplexInt32DType, value_in_2d)
+    value_out_3d = convert_complex(np.complex64, value_in_3d)
+
+    assert_type(value_out_1d, np.ndarray[tuple[int], np.dtype[np.complex128]])
+    assert_type(value_out_2d, np.ndarray[tuple[int, int], np.dtype[ComplexInt32Base]])
+    assert_type(value_out_3d, np.ndarray[tuple[int, int, int], np.dtype[np.complex64]])
+    assert isinstance(value_out_1d, np.ndarray) and value_out_1d.shape == (3,)
+    assert isinstance(value_out_2d, np.ndarray) and value_out_2d.shape == (4, 5)
+    assert isinstance(value_out_3d, np.ndarray) and value_out_3d.shape == (6, 7, 8)
+
+
 ###############################################################################
 # convert scalars
 ###############################################################################
