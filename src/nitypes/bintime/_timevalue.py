d263972f4aa56e7a1056f0ef6e266a8857cdde22
Brad Keryan <brad.keryan@ni.com>
2025-05-17 14:29:29 -0500

    bintime: Major refactor, still working out tests

--- src/nitypes/bintime/_timevalue.py
+++ src/nitypes/bintime/_timevalue.py
@@ -2,11 +2,23 @@ from __future__ import annotations
 
 import decimal
 import math
+import operator
 from decimal import Decimal
-from typing import Any, ClassVar, SupportsFloat, SupportsIndex, overload
+from functools import singledispatchmethod
+from typing import (
+    Any,
+    ClassVar,
+    Protocol,
+    SupportsIndex,
+    Union,
+    overload,
+    runtime_checkable,
+)
 
-from nitypes._arguments import arg_to_int, validate_unsupported_arg
-from nitypes._exceptions import add_note, invalid_arg_type
+from typing_extensions import Self, TypeAlias
+
+from nitypes._arguments import arg_to_int
+from nitypes._exceptions import invalid_arg_type
 
 _INT128_MAX = (1 << 127) - 1
 _INT128_MIN = -(1 << 127)
@@ -23,6 +35,18 @@ _PICOSECONDS_PER_SECOND = 1_000_000_000_000
 _FEMTOSECONDS_PER_SECOND = 1_000_000_000_000_000
 _ATTOSECONDS_PER_SECOND = 1_000_000_000_000_000_000
 
+_REPR_TICKS = True
+
+
+@runtime_checkable
+class _SupportsTotalSeconds(Protocol):
+    def total_seconds(self) -> float: ...
+
+
+@runtime_checkable
+class _SupportsPrecisionTotalSeconds(Protocol):
+    def precision_total_seconds(self) -> Decimal: ...
+
 
 class TimeValue:
     """A time value in NI Binary Time Format (NI-BTF).
@@ -35,57 +59,89 @@ class TimeValue:
     min: ClassVar[TimeValue]
     max: ClassVar[TimeValue]
 
+    _TimeValueLike: TypeAlias = Union[
+        "TimeValue", _SupportsTotalSeconds, _SupportsPrecisionTotalSeconds
+    ]
+
     __slots__ = ["_ticks"]
 
     _ticks: int
 
-    @overload
-    def __init__(  # noqa: D107 - missing docstring in __init__
-        self,
-        seconds: SupportsIndex | None = ...,
-        ticks: SupportsIndex | None = ...,
-    ) -> None: ...
-
-    @overload
-    def __init__(  # noqa: D107 - missing docstring in __init__
-        self,
-        seconds: SupportsFloat | Decimal,
-    ) -> None: ...
-
     def __init__(
         self,
-        seconds: SupportsIndex | SupportsFloat | None = None,
-        ticks: SupportsIndex | None = None,
+        seconds: (
+            SupportsIndex
+            | Decimal
+            | float
+            | _SupportsTotalSeconds
+            | _SupportsPrecisionTotalSeconds
+            | None
+        ) = None,
     ) -> None:
         """Initialize a TimeValue."""
-        self._ticks = 0
-        try:
-            if seconds is not None:
-                if isinstance(seconds, SupportsIndex):
-                    self._ticks += arg_to_int("seconds", seconds) * _TICKS_PER_SECOND
-                elif isinstance(seconds, Decimal):
-                    validate_unsupported_arg("ticks", ticks)
-                    whole_seconds, fractional_seconds = divmod(seconds, 1)
-                    self._ticks += int(whole_seconds) * _TICKS_PER_SECOND
-                    self._ticks += int(fractional_seconds * _TICKS_PER_SECOND)
-                elif isinstance(seconds, SupportsFloat):
-                    validate_unsupported_arg("ticks", ticks)
-                    fractional_seconds, whole_seconds = math.modf(seconds)
-                    self._ticks += int(whole_seconds) * _TICKS_PER_SECOND
-                    self._ticks += int(fractional_seconds * _TICKS_PER_SECOND)
-                else:
-                    raise invalid_arg_type("seconds", "integer or float", seconds)
-            if ticks is not None:
-                self._ticks += arg_to_int("ticks", ticks)
-            if not (_INT128_MIN <= self._ticks <= _INT128_MAX):
-                raise OverflowError("The time value is out of range.")
-        except Exception as e:
-            if seconds is not None:
-                add_note(e, f"Requested seconds: {seconds}")
-            if ticks is not None:
-                add_note(e, f"Requested ticks: {ticks}")
-            add_note(e, f"Calculated ticks: {self._ticks}")
-            raise
+        if seconds is None:
+            self._ticks = 0
+        else:
+            ticks = self.__class__._to_ticks(seconds)
+            if not (_INT128_MIN <= ticks <= _INT128_MAX):
+                raise OverflowError(
+                    "The seconds value is out of range.\n\n"
+                    f"Requested value: {seconds}\n"
+                    f"Minimum value: {self.__class__.min.precision_total_seconds()}\n"
+                    f"Maximum value: {self.__class__.max.precision_total_seconds()}"
+                )
+            self._ticks = ticks
+
+    @singledispatchmethod
+    @classmethod
+    def _to_ticks(cls, seconds: object) -> int:
+        raise invalid_arg_type("seconds", "number or timedelta", seconds)
+
+    @_to_ticks.register
+    @classmethod
+    def _(cls, seconds: SupportsIndex) -> int:
+        return operator.index(seconds) << _BITS_PER_SECOND
+
+    @_to_ticks.register
+    @classmethod
+    def _(cls, seconds: Decimal) -> int:
+        whole_seconds, fractional_seconds = divmod(seconds, 1)
+        ticks = int(whole_seconds) * _TICKS_PER_SECOND
+        ticks += int(fractional_seconds * _TICKS_PER_SECOND)
+        return ticks
+
+    @_to_ticks.register
+    @classmethod
+    def _(cls, seconds: float) -> int:
+        fractional_seconds, whole_seconds = math.modf(seconds)
+        ticks = int(whole_seconds) * _TICKS_PER_SECOND
+        ticks += int(fractional_seconds * _TICKS_PER_SECOND)
+        return ticks
+
+    @_to_ticks.register
+    @classmethod
+    def _(cls, seconds: _SupportsPrecisionTotalSeconds) -> int:
+        return cls._to_ticks(seconds.precision_total_seconds())
+
+    @_to_ticks.register
+    @classmethod
+    def _(cls, seconds: _SupportsTotalSeconds) -> int:
+        return cls._to_ticks(seconds.total_seconds())
+
+    @classmethod
+    def from_ticks(cls, ticks: SupportsIndex) -> Self:
+        """Create a TimeValue from a 128-bit fixed point number expressed as an integer."""
+        ticks = arg_to_int("ticks", ticks)
+        if not (_INT128_MIN <= ticks <= _INT128_MAX):
+            raise OverflowError(
+                "The ticks value is out of range.\n\n"
+                f"Requested value: {ticks}\n"
+                f"Minimum value: {_INT128_MIN}\n",
+                f"Maximum value: {_INT128_MAX}",
+            )
+        self = cls()
+        self._ticks = ticks
+        return self
 
     @property
     def days(self) -> int:
@@ -103,45 +159,29 @@ class TimeValue:
         return (1000 * self._ticks) >> _BITS_PER_SECOND
 
     def total_seconds(self) -> float:
-        """The total seconds in the time value."""
-        return (self._ticks >> _BITS_PER_SECOND) + (
-            (self._ticks & _FRACTIONAL_SECONDS_MASK) / _TICKS_PER_SECOND
-        )
+        """The total seconds in the time value.
+
+        .. warning::
+            Converting a time value to a floating point number loses precision.
+        """
+        seconds = float(self._ticks >> _BITS_PER_SECOND)
+        seconds += float((self._ticks & _FRACTIONAL_SECONDS_MASK) / _TICKS_PER_SECOND)
+        return seconds
 
     def precision_total_seconds(self) -> Decimal:
         """The precise total seconds in the time value.
 
-        Note: up to 64 significant digits are used in computation.
+        Note: up to 128 significant digits are used in computation.
         """
         with decimal.localcontext() as ctx:
-            ctx.prec = 64
-            return Decimal(self._ticks >> _BITS_PER_SECOND) + Decimal(
-                self._ticks & _FRACTIONAL_SECONDS_MASK
-            ) / Decimal(_TICKS_PER_SECOND)
-
-    def __add__(self, value: TimeValue, /) -> TimeValue:
-        """Return self+value."""
-        if not isinstance(value, TimeValue):
-            return NotImplemented
-        return TimeValue(ticks=self._ticks + value._ticks)
-
-    __radd__ = __add__
-
-    def __sub__(self, value: TimeValue, /) -> TimeValue:
-        """Return self-value."""
-        if not isinstance(value, TimeValue):
-            return NotImplemented
-        return TimeValue(ticks=self._ticks - value._ticks)
-
-    def __rsub__(self, value: TimeValue, /) -> TimeValue:
-        """Return value-self."""
-        if not isinstance(value, TimeValue):
-            return NotImplemented
-        return TimeValue(ticks=value._ticks - self._ticks)
+            ctx.prec = 128
+            seconds = Decimal(self._ticks >> _BITS_PER_SECOND)
+            seconds += Decimal(self._ticks & _FRACTIONAL_SECONDS_MASK) / Decimal(_TICKS_PER_SECOND)
+            return seconds
 
     def __neg__(self) -> TimeValue:
         """Return -self."""
-        return TimeValue(ticks=-self._ticks)
+        return self.__class__.from_ticks(-self._ticks)
 
     def __pos__(self) -> TimeValue:
         """Return +self."""
@@ -151,13 +191,44 @@ class TimeValue:
         """Return abs(self)."""
         return -self if self._ticks < 0 else self
 
-    def __mul__(self, value: float, /) -> TimeValue:
+    def __add__(self, value: _TimeValueLike, /) -> TimeValue:
+        """Return self+value."""
+        if isinstance(value, TimeValue):
+            return self.__class__.from_ticks(self._ticks + value._ticks)
+        elif isinstance(value, (_SupportsTotalSeconds, _SupportsPrecisionTotalSeconds)):
+            return self + TimeValue(value)
+        else:
+            return NotImplemented
+
+    __radd__ = __add__
+
+    def __sub__(self, value: _TimeValueLike, /) -> TimeValue:
+        """Return self-value."""
+        if isinstance(value, TimeValue):
+            return self.__class__.from_ticks(self._ticks - value._ticks)
+        elif isinstance(value, (_SupportsTotalSeconds, _SupportsPrecisionTotalSeconds)):
+            return self - TimeValue(value)
+        else:
+            return NotImplemented
+
+    def __rsub__(self, value: _TimeValueLike, /) -> TimeValue:
+        """Return value-self."""
+        if isinstance(value, TimeValue):
+            return self.__class__.from_ticks(value._ticks - self._ticks)
+        elif isinstance(value, (_SupportsTotalSeconds, _SupportsPrecisionTotalSeconds)):
+            return TimeValue(value) - self
+        else:
+            return NotImplemented
+
+    def __mul__(self, value: int | float | Decimal, /) -> TimeValue:
         """Return self*value."""
         if isinstance(value, int):
-            return TimeValue(ticks=self._ticks * value)
+            return self.__class__.from_ticks(self._ticks * value)
         elif isinstance(value, float):
             # Using floating point math on 128-bit numbers loses precision, so use Decimal math.
-            return TimeValue(self.precision_total_seconds() * Decimal(value))
+            return self * Decimal(value)
+        elif isinstance(value, Decimal):
+            return TimeValue(self.precision_total_seconds() * value)
         else:
             return NotImplemented
 
@@ -175,9 +246,9 @@ class TimeValue:
     def __floordiv__(self, value: TimeValue | int, /) -> int | TimeValue:
         """Return self//value."""
         if isinstance(value, TimeValue):
-            return TimeValue(ticks=self._ticks // value._ticks)
+            return self.__class__.from_ticks(self._ticks // value._ticks)
         elif isinstance(value, int):
-            return TimeValue(ticks=self._ticks // value)
+            return self.__class__.from_ticks(self._ticks // value)
         else:
             return NotImplemented
 
@@ -199,53 +270,68 @@ class TimeValue:
         else:
             return NotImplemented
 
-    def __mod__(self, value: TimeValue, /) -> TimeValue:
+    def __mod__(self, value: _TimeValueLike, /) -> TimeValue:
         """Return self%value."""
-        if not isinstance(value, TimeValue):
+        if isinstance(value, TimeValue):
+            return self.__class__.from_ticks(self._ticks % value._ticks)
+        elif isinstance(value, (_SupportsTotalSeconds, _SupportsPrecisionTotalSeconds)):
+            return self % TimeValue(value)
+        else:
             return NotImplemented
-        return TimeValue(ticks=self._ticks % value._ticks)
 
-    def __divmod__(self, value: TimeValue, /) -> tuple[int, TimeValue]:
+    def __divmod__(self, value: _TimeValueLike, /) -> tuple[int, TimeValue]:
         """Return (self//value, self%value)."""
-        if not isinstance(value, TimeValue):
+        if isinstance(value, TimeValue):
+            return (self // value, self % value)
+        elif isinstance(value, (_SupportsTotalSeconds, _SupportsPrecisionTotalSeconds)):
+            return divmod(self, TimeValue(value))
+        else:
             return NotImplemented
-        return (self // value, self % value)
 
-    def __lt__(self, value: TimeValue, /) -> bool:
+    def __lt__(self, value: _TimeValueLike, /) -> bool:
         """Return self<value."""
-        if not isinstance(value, self.__class__):
+        if isinstance(value, self.__class__):
+            return self._ticks < value._ticks
+        elif isinstance(value, (_SupportsTotalSeconds, _SupportsPrecisionTotalSeconds)):
+            return self < TimeValue(value)
+        else:
             return NotImplemented
-        return self._ticks < value._ticks
 
-    def __le__(self, value: TimeValue, /) -> bool:
+    def __le__(self, value: _TimeValueLike, /) -> bool:
         """Return self<=value."""
-        if not isinstance(value, self.__class__):
+        if isinstance(value, self.__class__):
+            return self._ticks <= value._ticks
+        elif isinstance(value, (_SupportsTotalSeconds, _SupportsPrecisionTotalSeconds)):
+            return self <= TimeValue(value)
+        else:
             return NotImplemented
-        return self._ticks <= value._ticks
 
     def __eq__(self, value: object, /) -> bool:
         """Return self==value."""
-        if not isinstance(value, self.__class__):
+        if isinstance(value, self.__class__):
+            return self._ticks == value._ticks
+        elif isinstance(value, (_SupportsTotalSeconds, _SupportsPrecisionTotalSeconds)):
+            return self == TimeValue(value)
+        else:
             return NotImplemented
-        return self._ticks == value._ticks
 
-    def __ne__(self, value: object, /) -> bool:
-        """Return self==value."""
-        if not isinstance(value, self.__class__):
-            return NotImplemented
-        return self._ticks != value._ticks
-
-    def __gt__(self, value: TimeValue, /) -> bool:
+    def __gt__(self, value: _TimeValueLike, /) -> bool:
         """Return self<value."""
-        if not isinstance(value, self.__class__):
+        if isinstance(value, self.__class__):
+            return self._ticks > value._ticks
+        elif isinstance(value, (_SupportsTotalSeconds, _SupportsPrecisionTotalSeconds)):
+            return self > TimeValue(value)
+        else:
             return NotImplemented
-        return self._ticks > value._ticks
 
-    def __ge__(self, value: TimeValue, /) -> bool:
+    def __ge__(self, value: _TimeValueLike, /) -> bool:
         """Return self>=value."""
-        if not isinstance(value, self.__class__):
+        if isinstance(value, self.__class__):
+            return self._ticks >= value._ticks
+        elif isinstance(value, (_SupportsTotalSeconds, _SupportsPrecisionTotalSeconds)):
+            return self >= TimeValue(value)
+        else:
             return NotImplemented
-        return self._ticks >= value._ticks
 
     def __bool__(self) -> bool:
         """Return bool(self)."""
@@ -257,7 +343,7 @@ class TimeValue:
 
     def __reduce__(self) -> tuple[Any, ...]:
         """Return object state for pickling."""
-        return (self.__class__, (None, self._ticks))
+        return (self.__class__.from_ticks, (self._ticks,))
 
     def __str__(self) -> str:
         """Return repr(self)."""
@@ -265,15 +351,19 @@ class TimeValue:
         seconds = self.seconds
         minutes, seconds = divmod(seconds, 60)
         hours, minutes = divmod(minutes, 60)
-        attoseconds = (self._ticks * _ATTOSECONDS_PER_SECOND) >> _BITS_PER_SECOND
+        _, fractional_seconds = divmod(self.precision_total_seconds(), 1)
         s = f"{days} days, " if days else ""
-        s += f"{hours}:{minutes:02}:{seconds:02}.{attoseconds:018}"
+        s += f"{hours}:{minutes:02}:{seconds:02}.{fractional_seconds}"
         return s
 
     def __repr__(self) -> str:
         """Return repr(self)."""
-        return f"{self.__class__.__module__}.{self.__class__.__name__}(ticks={self._ticks})"
+        if _REPR_TICKS:
+            return (
+                f"{self.__class__.__module__}.{self.__class__.__name__}.from_ticks({self._ticks})"
+            )
+        return f"{self.__class__.__module__}.{self.__class__.__name__}({self.precision_total_seconds()!r})"
 
 
-TimeValue.max = TimeValue(ticks=_INT128_MAX)
-TimeValue.min = TimeValue(ticks=_INT128_MIN)
+TimeValue.max = TimeValue.from_ticks(_INT128_MAX)
+TimeValue.min = TimeValue.from_ticks(_INT128_MIN)
