7e5a07543ff854317d9ec94edf8fea5d0a1834dd
Mike Prosser <107578831+mikeprosserni@users.noreply.github.com>
2025-06-30 13:14:56 -0500

    Fix issues with np.bool (#141)
    * configure pytest to fail on deprecation warnings
    
    * revert lock file
    
    * use _np_bool in _types.py
    
    * fix lint
    
    * update test_digital_waveform.py to use _np_bool
    
    * update test_digital_waveform_signal.py to use _np_bool
    
    * pyproject.toml: Use filterwarnings key
    
    ---------
    
    Co-authored-by: Mike Prosser <Mike.Prosser@emerson.com>
    Co-authored-by: Brad Keryan <brad.keryan@ni.com>

--- src/nitypes/waveform/_digital/_types.py
+++ src/nitypes/waveform/_digital/_types.py
@@ -5,6 +5,8 @@ from typing import TypeVar, Union
 import numpy as np
 from typing_extensions import TypeAlias
 
+from nitypes._numpy import bool as _np_bool
+
 __all__ = [
     "_AnyPort",
     "_AnyState",
@@ -17,9 +19,9 @@ __all__ = [
 _AnyPort: TypeAlias = Union[np.uint8, np.uint16, np.uint32]
 
 # np.byte == np.int8, np.ubyte == np.uint8
-_AnyState: TypeAlias = Union[np.bool, np.int8, np.uint8]
+_AnyState: TypeAlias = Union[_np_bool, np.int8, np.uint8]
 _TState = TypeVar("_TState", bound=_AnyState)
 _TOtherState = TypeVar("_TOtherState", bound=_AnyState)
 
 _DIGITAL_PORT_DTYPES = (np.uint8, np.uint16, np.uint32)
-_DIGITAL_STATE_DTYPES = (np.bool, np.int8, np.uint8)
+_DIGITAL_STATE_DTYPES = (_np_bool, np.int8, np.uint8)
