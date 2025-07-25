f35b7ff0ae3aa4ecb2d752874dbcc5e038057c26
Joe Friedrichsen <114173023+jfriedri-ni@users.noreply.github.com>
2025-07-25 13:49:31 -0700

    Add a test for indexing by integer
    Signed-off-by: Joe Friedrichsen <114173023+jfriedri-ni@users.noreply.github.com>

--- tests/unit/bintime/test_timedelta_array.py
+++ tests/unit/bintime/test_timedelta_array.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import pytest
 from typing_extensions import assert_type
 
 from nitypes.bintime import TimeDelta
@@ -24,3 +25,24 @@ def test___list_arg___construct___returns_matching_array() -> None:
     assert (value._array[0]["msb"], value._array[0]["lsb"]) == TimeDelta(-1).to_tuple()
     assert (value._array[1]["msb"], value._array[1]["lsb"]) == TimeDelta(20.26).to_tuple()
     assert (value._array[2]["msb"], value._array[2]["lsb"]) == TimeDelta(500).to_tuple()
+
+
+@pytest.mark.parametrize(
+    "timedelta_list,exception",
+    (
+        (None, IndexError()),
+        ([TimeDelta(3.14)], None),
+        ([TimeDelta(-1), TimeDelta(20.26), TimeDelta(500)], None),
+    ),
+)
+def test__timedelta_array___index_first___returns_timedelta(
+    timedelta_list: list[TimeDelta], exception: BaseException | None
+) -> None:
+    value = TimeDeltaArray(timedelta_list)
+
+    if exception:
+        with pytest.raises(type(exception)):
+            entry = value[0]
+    else:
+        entry = value[0]
+        assert entry == timedelta_list[0]
