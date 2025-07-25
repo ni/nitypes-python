5dda9f076db378bcf304cbf2d0bd22c215f2b45b
Joe Friedrichsen <114173023+jfriedri-ni@users.noreply.github.com>
2025-07-25 10:16:56 -0700

    Test with negative and fractional values as well
    Signed-off-by: Joe Friedrichsen <114173023+jfriedri-ni@users.noreply.github.com>

--- tests/unit/bintime/test_timedelta_array.py
+++ tests/unit/bintime/test_timedelta_array.py
@@ -15,12 +15,12 @@ def test___no_args___construct___returns_empty_array() -> None:
 
 
 def test___list_arg___construct___returns_matching_array() -> None:
-    arg = [TimeDelta(1), TimeDelta(20), TimeDelta(500)]
+    arg = [TimeDelta(-1), TimeDelta(20.26), TimeDelta(500)]
     value = TimeDeltaArray(arg)
 
     assert_type(value, TimeDeltaArray)
     assert isinstance(value, TimeDeltaArray)
     assert len(value._array) == len(arg)
-    assert (value._array[0]["msb"], value._array[0]["lsb"]) == TimeDelta(1).to_tuple()
-    assert (value._array[1]["msb"], value._array[1]["lsb"]) == TimeDelta(20).to_tuple()
+    assert (value._array[0]["msb"], value._array[0]["lsb"]) == TimeDelta(-1).to_tuple()
+    assert (value._array[1]["msb"], value._array[1]["lsb"]) == TimeDelta(20.26).to_tuple()
     assert (value._array[2]["msb"], value._array[2]["lsb"]) == TimeDelta(500).to_tuple()
