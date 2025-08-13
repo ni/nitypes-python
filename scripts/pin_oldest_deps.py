85f9e693a714b42d5dd8144f33d867110e775c1c
Michael Johansen <michael.johansen@ni.com>
2025-08-13 16:27:17 -0500

    Update the pin oldest deps script to also handle dev dependencies.
    Signed-off-by: Michael Johansen <michael.johansen@ni.com>

--- scripts/pin_oldest_deps.py
+++ scripts/pin_oldest_deps.py
@@ -15,16 +15,30 @@ def main(args: list[str]) -> int | str | None:
     if args:
         return f"Unsupported arguments: {args!r}"
     pyproject = tomlkit.loads(pyproject_path.read_text())
+
     poetry_deps = pyproject["tool"]["poetry"]["dependencies"]  # type: ignore[index]
     assert isinstance(poetry_deps, AbstractTable)
+    _pin_oldest_for_deps_list(poetry_deps)
 
-    for dep, value in poetry_deps.items():
+    dev_deps = pyproject["tool"]["poetry"]["group"]["dev"]["dependencies"]
+    assert isinstance(dev_deps, AbstractTable)
+    _pin_oldest_for_deps_list(dev_deps)
+
+    pyproject_path.write_text(tomlkit.dumps(pyproject))
+    print("Updated pyproject.toml with pinned dependencies.")
+    return None
+
+
+def _pin_oldest_for_deps_list(deps_list: AbstractTable) -> None:
+    assert isinstance(deps_list, AbstractTable)
+
+    for dep, value in deps_list.items():
         if dep == "python":
             continue
         if isinstance(value, str) and (
             value.startswith("^") or value.startswith("~") or value.startswith(">=")
         ):
-            poetry_deps[dep] = "==" + value.lstrip("^~>=")
+            deps_list[dep] = "==" + value.lstrip("^~>=")
         elif isinstance(value, Array):
             for constraint in value:
                 if "version" in constraint and (
@@ -33,10 +47,6 @@ def main(args: list[str]) -> int | str | None:
                     or constraint["version"].startswith(">=")
                 ):
                     constraint["version"] = "==" + constraint["version"].lstrip("^~>=")
-
-    pyproject_path.write_text(tomlkit.dumps(pyproject))
-    print("Updated pyproject.toml with pinned dependencies.")
-    return None
 
 
 if __name__ == "__main__":
