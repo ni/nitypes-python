d68334f0f4f38c712e5b702eaa04951f3af7f7d2
Joe Friedrichsen <114173023+jfriedri-ni@users.noreply.github.com>
2025-08-28 15:36:31 -0700

    Do not construct the long test lists
    Signed-off-by: Joe Friedrichsen <114173023+jfriedri-ni@users.noreply.github.com>

--- tests/benchmark/bintime/test_datetime_array.py
+++ tests/benchmark/bintime/test_datetime_array.py
@@ -10,19 +10,19 @@ import nitypes.bintime as bt
 LIST_10: list[bt.DateTime] = [
     bt.DateTime.from_offset(bt.TimeDelta(float(offset))) for offset in np.arange(0, 10, 0.3)
 ]
-LIST_100: list[bt.DateTime] = [
-    bt.DateTime.from_offset(bt.TimeDelta(float(offset))) for offset in np.arange(0, 100, 0.3)
-]
-LIST_1000: list[bt.DateTime] = [
-    bt.DateTime.from_offset(bt.TimeDelta(float(offset))) for offset in np.arange(0, 1000, 0.3)
-]
-LIST_10000: list[bt.DateTime] = [
-    bt.DateTime.from_offset(bt.TimeDelta(float(offset))) for offset in np.arange(0, 10000, 0.3)
-]
+# LIST_100: list[bt.DateTime] = [
+#     bt.DateTime.from_offset(bt.TimeDelta(float(offset))) for offset in np.arange(0, 100, 0.3)
+# ]
+# LIST_1000: list[bt.DateTime] = [
+#     bt.DateTime.from_offset(bt.TimeDelta(float(offset))) for offset in np.arange(0, 1000, 0.3)
+# ]
+# LIST_10000: list[bt.DateTime] = [
+#     bt.DateTime.from_offset(bt.TimeDelta(float(offset))) for offset in np.arange(0, 10000, 0.3)
+# ]
 
 
 @pytest.mark.benchmark(group="datetime_array_construct", min_rounds=1, max_time=0.5)
-@pytest.mark.parametrize("constructor_list", (LIST_10, LIST_100, LIST_1000, LIST_10000))
+@pytest.mark.parametrize("constructor_list", (LIST_10))
 def test___bt_datetime_array___construct(
     benchmark: BenchmarkFixture,
     constructor_list: list[bt.DateTime],
@@ -31,7 +31,7 @@ def test___bt_datetime_array___construct(
 
 
 @pytest.mark.benchmark(group="datetime_array_extend", min_rounds=1, max_time=0.5)
-@pytest.mark.parametrize("extend_list", (LIST_10, LIST_100, LIST_1000, LIST_10000))
+@pytest.mark.parametrize("extend_list", (LIST_10))
 def test___bt_datetime_array___extend(
     benchmark: BenchmarkFixture,
     extend_list: list[bt.DateTime],
--- tests/benchmark/bintime/test_timedelta_array.py
+++ tests/benchmark/bintime/test_timedelta_array.py
@@ -8,17 +8,17 @@ import nitypes.bintime as bt
 
 
 LIST_10: list[bt.TimeDelta] = [bt.TimeDelta(float(value)) for value in np.arange(-10, 10, 0.3)]
-LIST_100: list[bt.TimeDelta] = [bt.TimeDelta(float(value)) for value in np.arange(-100, 100, 0.3)]
-LIST_1000: list[bt.TimeDelta] = [
-    bt.TimeDelta(float(value)) for value in np.arange(-1000, 1000, 0.3)
-]
-LIST_10000: list[bt.TimeDelta] = [
-    bt.TimeDelta(float(value)) for value in np.arange(-10000, 10000, 0.3)
-]
+# LIST_100: list[bt.TimeDelta] = [bt.TimeDelta(float(value)) for value in np.arange(-100, 100, 0.3)]
+# LIST_1000: list[bt.TimeDelta] = [
+#     bt.TimeDelta(float(value)) for value in np.arange(-1000, 1000, 0.3)
+# ]
+# LIST_10000: list[bt.TimeDelta] = [
+#     bt.TimeDelta(float(value)) for value in np.arange(-10000, 10000, 0.3)
+# ]
 
 
 @pytest.mark.benchmark(group="timedelta_array_construct", min_rounds=1, max_time=0.5)
-@pytest.mark.parametrize("constructor_list", (LIST_10, LIST_100, LIST_1000, LIST_10000))
+@pytest.mark.parametrize("constructor_list", (LIST_10))
 def test___bt_timedelta_array___construct(
     benchmark: BenchmarkFixture,
     constructor_list: list[bt.TimeDelta],
@@ -27,7 +27,7 @@ def test___bt_timedelta_array___construct(
 
 
 @pytest.mark.benchmark(group="timedelta_array_extend", min_rounds=1, max_time=0.5)
-@pytest.mark.parametrize("extend_list", (LIST_10, LIST_100, LIST_1000, LIST_10000))
+@pytest.mark.parametrize("extend_list", (LIST_10))
 def test___bt_timedelta_array___extend(
     benchmark: BenchmarkFixture,
     extend_list: list[bt.TimeDelta],
