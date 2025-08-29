41e6dddcb5468632be181f375c625b259801704a
Joe F <114173023+jfriedri-ni@users.noreply.github.com>
2025-08-29 16:51:35 -0500

    Fix #182 and reduce benchmark test times (#184)
    - Add and use a length-1 list test case by default for the array tests
    - Detect PyPy and use additional benchmark options
    - Remove type hints from constants
    
    Signed-off-by: Joe Friedrichsen <114173023+jfriedri-ni@users.noreply.github.com>

--- tests/benchmark/bintime/test_datetime_array.py
+++ tests/benchmark/bintime/test_datetime_array.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+import sys
+from typing import Any
+
 import numpy as np
 import pytest
 from pytest_benchmark.fixture import BenchmarkFixture
@@ -7,22 +10,32 @@ from pytest_benchmark.fixture import BenchmarkFixture
 import nitypes.bintime as bt
 
 
-LIST_10: list[bt.DateTime] = [
-    bt.DateTime.from_offset(bt.TimeDelta(float(offset))) for offset in np.arange(0, 10, 0.3)
-]
-LIST_100: list[bt.DateTime] = [
+LIST_1 = [bt.DateTime.from_offset(bt.TimeDelta(0.3))]
+LIST_10 = [bt.DateTime.from_offset(bt.TimeDelta(float(offset))) for offset in np.arange(0, 10, 0.3)]
+LIST_100 = [
     bt.DateTime.from_offset(bt.TimeDelta(float(offset))) for offset in np.arange(0, 100, 0.3)
 ]
-LIST_1000: list[bt.DateTime] = [
+LIST_1000 = [
     bt.DateTime.from_offset(bt.TimeDelta(float(offset))) for offset in np.arange(0, 1000, 0.3)
 ]
-LIST_10000: list[bt.DateTime] = [
+LIST_10000 = [
     bt.DateTime.from_offset(bt.TimeDelta(float(offset))) for offset in np.arange(0, 10000, 0.3)
 ]
 
+FAST_CASES = (LIST_1,)
+BIG_O_CASES = (LIST_1, LIST_10, LIST_100, LIST_1000, LIST_10000)  # Useful for local measurements
 
-@pytest.mark.benchmark(group="datetime_array_construct", min_rounds=100)
-@pytest.mark.parametrize("constructor_list", (LIST_10, LIST_100, LIST_1000, LIST_10000))
+
+benchmark_options: dict[str, Any] = {}
+if sys.implementation.name == "pypy":
+    # See #182 -- PR/CI workflows spend too much time on PyPy benchmarks
+    benchmark_options["warmup"] = False
+    benchmark_options["min_rounds"] = 1
+    benchmark_options["max_time"] = 0.5
+
+
+@pytest.mark.benchmark(group="datetime_array_construct", **benchmark_options)
+@pytest.mark.parametrize("constructor_list", FAST_CASES)
 def test___bt_datetime_array___construct(
     benchmark: BenchmarkFixture,
     constructor_list: list[bt.DateTime],
@@ -30,8 +43,8 @@ def test___bt_datetime_array___construct(
     benchmark(bt.DateTimeArray, constructor_list)
 
 
-@pytest.mark.benchmark(group="datetime_array_extend", min_rounds=100)
-@pytest.mark.parametrize("extend_list", (LIST_10, LIST_100, LIST_1000, LIST_10000))
+@pytest.mark.benchmark(group="datetime_array_extend", **benchmark_options)
+@pytest.mark.parametrize("extend_list", FAST_CASES)
 def test___bt_datetime_array___extend(
     benchmark: BenchmarkFixture,
     extend_list: list[bt.DateTime],
--- tests/benchmark/bintime/test_timedelta_array.py
+++ tests/benchmark/bintime/test_timedelta_array.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+import sys
+from typing import Any
+
 import numpy as np
 import pytest
 from pytest_benchmark.fixture import BenchmarkFixture
@@ -7,18 +10,26 @@ from pytest_benchmark.fixture import BenchmarkFixture
 import nitypes.bintime as bt
 
 
-LIST_10: list[bt.TimeDelta] = [bt.TimeDelta(float(value)) for value in np.arange(-10, 10, 0.3)]
-LIST_100: list[bt.TimeDelta] = [bt.TimeDelta(float(value)) for value in np.arange(-100, 100, 0.3)]
-LIST_1000: list[bt.TimeDelta] = [
-    bt.TimeDelta(float(value)) for value in np.arange(-1000, 1000, 0.3)
-]
-LIST_10000: list[bt.TimeDelta] = [
-    bt.TimeDelta(float(value)) for value in np.arange(-10000, 10000, 0.3)
-]
+LIST_1 = [bt.TimeDelta(0.3)]
+LIST_10 = [bt.TimeDelta(float(value)) for value in np.arange(-10, 10, 0.3)]
+LIST_100 = [bt.TimeDelta(float(value)) for value in np.arange(-100, 100, 0.3)]
+LIST_1000 = [bt.TimeDelta(float(value)) for value in np.arange(-1000, 1000, 0.3)]
+LIST_10000 = [bt.TimeDelta(float(value)) for value in np.arange(-10000, 10000, 0.3)]
+
+FAST_CASES = (LIST_1,)
+BIG_O_CASES = (LIST_1, LIST_10, LIST_100, LIST_1000, LIST_10000)  # Useful for local measurements
 
 
-@pytest.mark.benchmark(group="timedelta_array_construct", min_rounds=100)
-@pytest.mark.parametrize("constructor_list", (LIST_10, LIST_100, LIST_1000, LIST_10000))
+benchmark_options: dict[str, Any] = {}
+if sys.implementation.name == "pypy":
+    # See #182 -- PR/CI workflows spend too much time on PyPy benchmarks
+    benchmark_options["warmup"] = False
+    benchmark_options["min_rounds"] = 1
+    benchmark_options["max_time"] = 0.5
+
+
+@pytest.mark.benchmark(group="timedelta_array_construct", **benchmark_options)
+@pytest.mark.parametrize("constructor_list", FAST_CASES)
 def test___bt_timedelta_array___construct(
     benchmark: BenchmarkFixture,
     constructor_list: list[bt.TimeDelta],
@@ -26,8 +37,8 @@ def test___bt_timedelta_array___construct(
     benchmark(bt.TimeDeltaArray, constructor_list)
 
 
-@pytest.mark.benchmark(group="timedelta_array_extend", min_rounds=100)
-@pytest.mark.parametrize("extend_list", (LIST_10, LIST_100, LIST_1000, LIST_10000))
+@pytest.mark.benchmark(group="timedelta_array_extend", **benchmark_options)
+@pytest.mark.parametrize("extend_list", FAST_CASES)
 def test___bt_timedelta_array___extend(
     benchmark: BenchmarkFixture,
     extend_list: list[bt.TimeDelta],
