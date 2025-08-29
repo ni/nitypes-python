052409851f52454269410eaaf1a45acf09f8553b
Joe Friedrichsen <114173023+jfriedri-ni@users.noreply.github.com>
2025-08-29 14:19:46 -0700

    Constrain benchmarks only for PyPy
    Signed-off-by: Joe Friedrichsen <114173023+jfriedri-ni@users.noreply.github.com>

--- tests/benchmark/bintime/test_datetime_array.py
+++ tests/benchmark/bintime/test_datetime_array.py
@@ -1,10 +1,24 @@
 from __future__ import annotations
 
+import sys
+from typing import Any
+
 import numpy as np
 import pytest
 from pytest_benchmark.fixture import BenchmarkFixture
 
 import nitypes.bintime as bt
+
+
+benchmark_options: dict[str, Any] = {}
+match sys.implementation.name:
+    case "pypy":
+        # See #182 -- PR/CI workflows spend too much time on PyPy benchmarks
+        benchmark_options["warmup"] = False
+        benchmark_options["min_rounds"] = 1
+        benchmark_options["max_time"] = 0.5
+    case _:
+        pass
 
 
 LIST_1 = [bt.DateTime.from_offset(bt.TimeDelta(0.3))]
@@ -19,11 +33,11 @@ LIST_10000 = [
     bt.DateTime.from_offset(bt.TimeDelta(float(offset))) for offset in np.arange(0, 10000, 0.3)
 ]
 
-FAST_CASES = (LIST_1, LIST_10, LIST_100)
+FAST_CASES = (LIST_1,)
 BIG_O_CASES = (LIST_1, LIST_10, LIST_100, LIST_1000, LIST_10000)
 
 
-@pytest.mark.benchmark(group="datetime_array_construct")
+@pytest.mark.benchmark(group="datetime_array_construct", **benchmark_options)
 @pytest.mark.parametrize("constructor_list", FAST_CASES)
 def test___bt_datetime_array___construct(
     benchmark: BenchmarkFixture,
@@ -32,7 +46,7 @@ def test___bt_datetime_array___construct(
     benchmark(bt.DateTimeArray, constructor_list)
 
 
-@pytest.mark.benchmark(group="datetime_array_extend")
+@pytest.mark.benchmark(group="datetime_array_extend", **benchmark_options)
 @pytest.mark.parametrize("extend_list", FAST_CASES)
 def test___bt_datetime_array___extend(
     benchmark: BenchmarkFixture,
--- tests/benchmark/bintime/test_timedelta_array.py
+++ tests/benchmark/bintime/test_timedelta_array.py
@@ -1,10 +1,24 @@
 from __future__ import annotations
 
+import sys
+from typing import Any
+
 import numpy as np
 import pytest
 from pytest_benchmark.fixture import BenchmarkFixture
 
 import nitypes.bintime as bt
+
+
+benchmark_options: dict[str, Any] = {}
+match sys.implementation.name:
+    case "pypy":
+        # See #182 -- PR/CI workflows spend too much time on PyPy benchmarks
+        benchmark_options["warmup"] = False
+        benchmark_options["min_rounds"] = 1
+        benchmark_options["max_time"] = 0.5
+    case _:
+        pass
 
 
 LIST_1 = [bt.TimeDelta(0.3)]
@@ -14,10 +28,10 @@ LIST_1000 = [bt.TimeDelta(float(value)) for value in np.arange(-1000, 1000, 0.3)
 LIST_10000 = [bt.TimeDelta(float(value)) for value in np.arange(-10000, 10000, 0.3)]
 
 FAST_CASES = (LIST_1, LIST_10)
-BIG_O_CASES = (LIST_1, LIST_10, LIST_100, LIST_1000, LIST_10000)
+BIG_O_CASES = (LIST_1, LIST_10, LIST_100, LIST_1000, LIST_10000)  # Useful for local measurements
 
 
-@pytest.mark.benchmark(group="timedelta_array_construct")
+@pytest.mark.benchmark(group="timedelta_array_construct", **benchmark_options)
 @pytest.mark.parametrize("constructor_list", FAST_CASES)
 def test___bt_timedelta_array___construct(
     benchmark: BenchmarkFixture,
@@ -26,7 +40,7 @@ def test___bt_timedelta_array___construct(
     benchmark(bt.TimeDeltaArray, constructor_list)
 
 
-@pytest.mark.benchmark(group="timedelta_array_extend")
+@pytest.mark.benchmark(group="timedelta_array_extend", **benchmark_options)
 @pytest.mark.parametrize("extend_list", FAST_CASES)
 def test___bt_timedelta_array___extend(
     benchmark: BenchmarkFixture,
