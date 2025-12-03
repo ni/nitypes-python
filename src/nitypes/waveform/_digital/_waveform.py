9f5f60dc48f788ca75d571a77dbb4eb0e82b9d0a
Mike Prosser <Mike.Prosser@emerson.com>
2025-12-03 09:46:59 -0600

    italics for 'See "Signal index vs. signal column index"' notes

--- src/nitypes/waveform/_digital/_waveform.py
+++ src/nitypes/waveform/_digital/_waveform.py
@@ -130,8 +130,8 @@ class DigitalWaveform(Generic[TDigitalState]):
     To construct a digital waveform from a NumPy array of line data, use the
     :any:`DigitalWaveform.from_lines` method. Each array element represents a digital state, such as 1
     for "on" or 0 for "off". The line data should be in a 1D array indexed by sample or a 2D array
-    indexed by (sample, signal). (Note, signal indices are reversed! See "Signal index vs. signal column index"
-    below for details.) The digital waveform displays the line data as a 2D array.
+    indexed by (sample, signal). *(Note, signal indices are reversed! See "Signal index vs. signal column index"
+    below for details.)* The digital waveform displays the line data as a 2D array.
 
     >>> import numpy as np
     >>> DigitalWaveform.from_lines(np.array([0, 1, 0], np.uint8))
@@ -148,8 +148,8 @@ class DigitalWaveform(Generic[TDigitalState]):
     To construct a digital waveform from a NumPy array of port data, use the
     :any:`DigitalWaveform.from_port` method. Each element of the port data array represents a digital
     sample taken over a port of signals. Each bit in the sample is a signal value, either 1 for "on" or
-    0 for "off". (Note, signal indices are reversed! See "Signal index vs. signal column index" below for
-    details.)
+    0 for "off". *(Note, signal indices are reversed! See "Signal index vs. signal column index" below for
+    details.)*
 
     >>> DigitalWaveform.from_port(np.array([0, 1, 2, 3], np.uint8))  # doctest: +NORMALIZE_WHITESPACE
     nitypes.waveform.DigitalWaveform(4, 8, data=array([[0, 0, 0, 0, 0, 0, 0, 0],
