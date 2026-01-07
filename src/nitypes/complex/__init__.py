47125f308577ed10bd72d7ea67651e469054780a
Kosta Ilic <kosta.ilic@emerson.com>
2026-01-07 06:48:42 -0600

    Fix black formatting (BLK100)
    Applied black formatting to resolve BLK100 styleguide issue in complex module.
    
    Signed-off-by: Kosta Ilic <kosta.ilic@emerson.com>

--- src/nitypes/complex/__init__.py
+++ src/nitypes/complex/__init__.py
@@ -135,4 +135,3 @@ from nitypes.complex._conversion import convert_complex
 from nitypes.complex._dtypes import ComplexInt32Base, ComplexInt32DType
 
 __all__ = ["convert_complex", "ComplexInt32DType", "ComplexInt32Base"]
-
